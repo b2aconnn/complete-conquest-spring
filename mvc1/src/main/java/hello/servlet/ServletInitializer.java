8b4b1acb9c854def338fd24b348cafbc1f4ebf0c
HJ Park <b2aconnn@gmail.com>
2024-08-25 15:52:23 +0900

    feat(mvc1 - section 7): 로깅 간단히 알아보기

--- mvc1/src/main/java/hello/servlet/ServletInitializer.java
+++ mvc1/src/main/java/hello/servlet/ServletInitializer.java
@@ -1,5 +1,6 @@
 package hello.servlet;
 
+import hello.Mvc1Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
