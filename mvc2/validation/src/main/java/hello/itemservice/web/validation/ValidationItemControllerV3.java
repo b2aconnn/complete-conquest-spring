fc7f0147bfd5ed301641f0ea5fc2c85ac3ff8a7c
HJ Park <b2aconnn@gmail.com>
2024-09-17 15:33:00 +0900

    feat(mvc2 - section 6): Bean Validation - groups

--- mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -45,9 +47,35 @@ public class ValidationItemControllerV3 {
         return "validation/v3/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult,
                           RedirectAttributes redirectAttributes, Model model) {
+
+        // Object Error 같은 경우, @ScriptAssert 로 스크립트 체크가 가능하지만, 로직으로 작성하는 걸 더 권장!!
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10_000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10_000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors : {}", bindingResult);
+
+            return "validation/v3/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItem2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes, Model model) {
 
         // Object Error 같은 경우, @ScriptAssert 로 스크립트 체크가 가능하지만, 로직으로 작성하는 걸 더 권장!!
         if (item.getPrice() != null && item.getQuantity() != null) {
@@ -78,9 +106,31 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
-    @PostMapping("/{itemId}/edit")
+//    @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId,
                        @Validated @ModelAttribute Item item,
+                       BindingResult bindingResult) {
+
+        // Object Error 같은 경우, @ScriptAssert 로 스크립트 체크가 가능하지만, 로직으로 작성하는 걸 더 권장!!
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10_000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10_000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit2(@PathVariable Long itemId,
+                       @Validated(UpdateCheck.class) @ModelAttribute Item item,
                        BindingResult bindingResult) {
 
         // Object Error 같은 경우, @ScriptAssert 로 스크립트 체크가 가능하지만, 로직으로 작성하는 걸 더 권장!!
