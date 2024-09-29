82e23db5cb72848c8fff85d2f9ffdd4e206d6edc
HJ Park <b2aconnn@gmail.com>
2024-09-29 17:32:36 +0900

    feat(mvc2 - section 8): 스프링 인터셉터 - 인증 체크

--- mvc2/login/src/main/java/hello/login/WebConfig.java
+++ mvc2/login/src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,10 +17,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 인터셉터의 장점 : pattern을 세밀하게 지정해 줄 수 있다. 서블릿 필터를 사용할 경우, 직접 list 등 만들어서
+        // 로직으로 체크해줘야 한다. 서블릿 필터에 비해 코드가 매우 간결해진다.
         registry.addInterceptor(new LogInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
     }
 
 //    @Bean
@@ -32,7 +41,7 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
