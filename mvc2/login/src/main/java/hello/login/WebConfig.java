50b0654893ca72c104c1656f5f555b04f1de9151
HJ Park <b2aconnn@gmail.com>
2024-09-29 14:41:27 +0900

    feat(mvc2 - section 8): 서블릿 필터 - 요청 로그

---
+++ mvc2/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,21 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1); // filter chain 에서 호출 순서를 지정.
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
