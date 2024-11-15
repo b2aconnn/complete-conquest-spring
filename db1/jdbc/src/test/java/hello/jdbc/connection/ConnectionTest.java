1263dbd550a3d0caf0268f586780fafe06c91545
HJ Park <b2aconnn@gmail.com>
2024-11-15 14:21:22 +0900

    feat(db1 - section 3): DataSource 예제2 - 커넥션 풀

--- db1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ db1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -30,7 +31,23 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // spring 에서 jdbc를 사용하게 되면 HikariCP를 주입 받게 된다.
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(3000);
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
+        // 만약 커넥션 풀에 커넥션이 만들어지지 않은 상태에서 Connection을 요청하면 풀에 생성이 될 때까지 기다렸다가 완료되면 가져올 수 있음
+        // Pool 1개 있을 경우, 1개 요청은 처리 가능. 2개 요청 시 Pool 에 2개 생성될 떄까지 1개 요청은 대기
         Connection connection1 = dataSource.getConnection();
         Connection connection2 = dataSource.getConnection();
 
