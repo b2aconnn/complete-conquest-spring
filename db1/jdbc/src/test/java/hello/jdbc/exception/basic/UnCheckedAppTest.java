b637c7608383de7ecd8591023420974cbd479540
HJ Park <b2aconnn@gmail.com>
2024-11-24 20:51:48 +0900

    feat(db1 - section 5): 예외 포함과 스택 트레이스

--- db1/jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ db1/jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
@@ -7,12 +8,24 @@ import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Slf4j
 public class UnCheckedAppTest {
     @Test
     void unchecked() {
         Controller controller = new Controller();
         assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            // e.printStackTrace() 를 사용하지 말고, log.info() 를 사용해서 출력하기
+            log.info("", e);
+        }
     }
 
     static class Controller {
