efa6198be1ef0672e94d3845f43c9bd6958965e4
HJ Park <b2aconnn@gmail.com>
2024-11-28 16:20:53 +0900

    feat(db2 - section 3): JdbcTemplate 적용1 - 기본

---
+++ db2/itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -0,0 +1,114 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.mapper.Mapper;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * JdbcTemplate
+ */
+@Slf4j
+public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    // jdbcTemplate 에서 connection 등을 생성해야 하기 때문에 DB 정보가 포함된 DataSource가 필요함
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        String sql = "INSERT INTO item (item_name, price, quantity) VALUES (?, ?, ?)";
+        // JdbcTemplate 에서 key를 생성하기 위해 사용
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+             PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+             ps.setString(1, item.getItemName());
+             ps.setDouble(2, item.getPrice());
+             ps.setInt(3, item.getQuantity());
+             return ps;
+        }, keyHolder);
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "UPDATE item SET item_name = ?, price = ?, quantity = ? WHERE id = ?";
+        jdbcTemplate.update(sql,
+                updateParam.getItemName(),
+                updateParam.getPrice(),
+                updateParam.getQuantity(),
+                itemId);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "SELECT * FROM item WHERE id = ?";
+        try {
+            Item item = jdbcTemplate.queryForObject(sql, itemRowMapper(), id);
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return (rs, rowNum) -> {
+            Item item = new Item();
+             item.setId(rs.getLong("id"));
+            item.setItemName(rs.getString("item_name"));
+            item.setPrice(rs.getInt("price"));
+            item.setQuantity(rs.getInt("quantity"));
+            return item;
+        };
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        String sql = "SELECT * FROM item";
+
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+        boolean andFlag = false;
+        List<Object> param = new ArrayList<>();
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',?,'%')";
+            param.add(itemName);
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= ?";
+            param.add(maxPrice);
+        }
+
+        log.info("sql={}", sql);
+        return jdbcTemplate.query(sql, itemRowMapper(), param.toArray());
+    }
+} 
