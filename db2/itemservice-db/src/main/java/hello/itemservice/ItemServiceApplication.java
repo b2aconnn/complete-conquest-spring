cf6a757a72d11f33c9b7c1080807effeb68132de
HJ Park <b2aconnn@gmail.com>
2024-12-03 15:25:05 +0900

    feat(db2 - section 4): 테스트 - 스프링 부트와 임베디드 모드

--- db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -31,19 +31,20 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
-	@Bean
-	@Profile("test")
-	public DataSource testDataSource() {
-		log.info("메모리 데이터베이스 초기화");
-
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		// jdbc:h2:mem:db : 임베디드 모드 (메모리 모드) 로 동작하는 H2 DB를 사용할 수 있다.
-		// DB_CLOSE_DELAY=-1 : 임베디드 모드에서 DB connection 이 모두 끊어지면 DB가 종료되는 것을 방지하기 위한 설정
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+	// test 에서 Datasource 를 지정하지 않으면 spring boot 에서는 임베디드 DB(memory H2 DB) 로 auto configuration 을 한다.
+//	@Bean
+//	@Profile("test")
+//	public DataSource testDataSource() {
+//		log.info("메모리 데이터베이스 초기화");
+//
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");
+//		// jdbc:h2:mem:db : 임베디드 모드 (메모리 모드) 로 동작하는 H2 DB를 사용할 수 있다.
+//		// DB_CLOSE_DELAY=-1 : 임베디드 모드에서 DB connection 이 모두 끊어지면 DB가 종료되는 것을 방지하기 위한 설정
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
 }
