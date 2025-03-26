7fa45560c94b9f209f8973ee00dd3015507b8371
HJ Park <b2aconnn@gmail.com>
2025-03-26 10:44:26 +0900

    feat(db2 - section 12): 트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ db2/springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -0,0 +1,44 @@
+package hello.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+    public void joinV1(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository call ==");
+        memberRepository.save(member);
+        log.info("== memberRepository exit ==");
+
+        log.info("== logRepository call ==");
+        logRepository.save(logMessage);
+        log.info("== logRepository exit ==");
+    }
+
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username) ;
+
+        log.info("== memberRepository call ==");
+        memberRepository.save(member);
+        log.info("== memberRepository exit ==");
+
+        log.info("== logRepository call ==");
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("failed log save. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름 반환");
+        }
+        log.info("== logRepository exit ==");
+    }
+}
