061cad10a2b3400ee412d5d195c6ae96babc96f1
HJ Park <b2aconnn@gmail.com>
2025-04-01 14:42:23 +0900

    feat(db2 - section 12): 트랜잭션 전파 활용6 - 복구 REQUIRED

--- db2/springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ db2/springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -26,6 +26,7 @@ public class MemberService {
         log.info("== logRepository exit ==");
     }
 
+    @Transactional
     public void joinV2(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username) ;
