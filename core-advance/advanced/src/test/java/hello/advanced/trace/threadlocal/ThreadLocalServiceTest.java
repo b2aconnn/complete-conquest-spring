48982c23a1a9da2cc370598ebb495c09e6719266
HJ Park <b2aconnn@gmail.com>
2025-04-05 14:11:11 +0900

    feat(core-advance - section 3): ThreadLocal - 예제 코드

---
+++ core-advance/advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void test() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        Thread threadB = new Thread(userB);
+
+        threadA.start();
+//        sleep(2000);
+//        sleep(100); // 2초로 지정하면 동시에 스레드가 동작하지 않기 때문에 동시성 문제 X
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
