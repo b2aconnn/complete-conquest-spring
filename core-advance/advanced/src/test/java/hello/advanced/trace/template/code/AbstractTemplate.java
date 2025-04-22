6693c703620a1c1a500de9ae16ef63e10f8eeb24
HJ Park <b2aconnn@gmail.com>
2025-04-22 14:37:55 +0900

    feat(core-advance - section 4): 템플릿 메서드 패턴 - 예제3

--- core-advance/advanced/src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
+++ core-advance/advanced/src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class AbstractTemplate {
-    private void execute() {
+    public void execute() {
         long startTime = System.currentTimeMillis();
 
         // 비즈니스 로직 실행
