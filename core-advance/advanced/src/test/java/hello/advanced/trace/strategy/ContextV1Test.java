1d1559c07a6114d13b3346196c8bdaf5d5922549
HJ Park <b2aconnn@gmail.com>
2025-04-22 16:00:06 +0900

    feat(core-advance - section 4): 전략 패턴 - 예제1

--- core-advance/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ core-advance/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,9 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -35,5 +39,19 @@ public class ContextV1Test {
 
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
+    }
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 ctx1 = new ContextV1(strategyLogic1);
+        ctx1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 ctx2 = new ContextV1(strategyLogic2);
+        ctx2.execute();
     }
 }
