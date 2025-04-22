5674915c5b13f2253b5380570cdcbc1bd8f0d740
HJ Park <b2aconnn@gmail.com>
2025-04-22 16:19:47 +0900

    feat(core-advance - section 4): 전략 패턴 - 예제2

--- core-advance/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ core-advance/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -4,6 +4,7 @@ import hello.advanced.trace.strategy.code.strategy.ContextV1;
 import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import io.micrometer.observation.Observation;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -53,5 +54,27 @@ public class ContextV1Test {
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 ctx2 = new ContextV1(strategyLogic2);
         ctx2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+        Strategy strategy1 = () -> log.info("call business logic 1");
+        ContextV1 contextV1 = new ContextV1(strategy1);
+        log.info("strategy1 = {}", strategy1.getClass());
+        contextV1.execute();
+
+        Strategy strategy2 = () -> log.info("call business logic 2");
+        ContextV1 contextV2 = new ContextV1(strategy2);
+        log.info("strategy2 = {}", strategy2.getClass());
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(() -> log.info("call business logic 1"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("call business logic 2"));
+        contextV2.execute();
     }
 }
