e0f0235a740ea318008e153c4003a633460de719
HJ Park <b2aconnn@gmail.com>
2025-05-12 15:50:25 +0900

    feat(core-advance - section 5): 구체 클래스 기반 프록시 - 예제2

--- core-advance/proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteTest.java
+++ core-advance/proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteTest.java
@@ -2,13 +2,23 @@ package hello.proxy.pureproxy.concreteproxy;
 
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteTest {
     @Test
     void noProxy() {
-         ConcreteLogic log ic = new ConcreteLogic();
+         ConcreteLogic logic = new ConcreteLogic();
          ConcreteClient client = new ConcreteClient(logic);
          client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic logic = new ConcreteLogic();
+        ConcreteLogic timeProxy = new TimeProxy(logic);
+
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
     }
 }
