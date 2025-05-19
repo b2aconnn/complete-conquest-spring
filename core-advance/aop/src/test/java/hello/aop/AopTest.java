06c61862415cd43e2731e5747e119a947a3af7bb
HJ Park <b2aconnn@gmail.com>
2025-05-19 11:16:18 +0900

    feat(core-advance - section 11): 스프링 AOP 구현4 - 포인트컷 참조

--- core-advance/aop/src/test/java/hello/aop/AopTest.java
+++ core-advance/aop/src/test/java/hello/aop/AopTest.java
@@ -3,6 +3,9 @@ package hello.aop;
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -14,7 +17,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV1.class)
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
     @Autowired
     OrderService orderService;
